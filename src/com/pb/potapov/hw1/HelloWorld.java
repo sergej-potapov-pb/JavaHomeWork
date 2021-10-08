4131749ecdf9d158713b36899eb6e88700c850a8
sergej-potapov-pb <sergej.potapov@privatbank.ua>
2021-10-09 01:49:29 +0300

    Размещение изменения проекта на GitHUB

--- src/com/pb/potapov/hw1/HelloWorld.java
+++ src/com/pb/potapov/hw1/HelloWorld.java
@@ -3,5 +3,6 @@ package com.pb.potapov.hw1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println("HelloWorld - new update");
     }
 }
