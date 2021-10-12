28a5e52fd5a47d004c24626f413d8d6ab6ba908e
sergej-potapov-pb <sergej.potapov@privatbank.ua>
2021-10-12 23:50:08 +0300

    Тестовый Каркулятор

---
+++ src/com/pb/potapov/hw2/Calculator.java
@@ -0,0 +1,40 @@
+package com.pb.potapov.hw2;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int a, b;
+        int res =0;
+        String sign;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Каркулятор\nВычисление арифметических функций : + - * /\nМежду двумя целыми числами\n");
+        System.out.print("Введите первое число ->");
+        a = scan.nextInt();
+        System.out.print("Введите второе число ->");
+        b = scan.nextInt();
+        System.out.println("Задайте операцию ->  + - * /");
+        sign = scan.next();
+
+        switch( sign ) {
+            case "+":
+                res = a + b;
+                break;
+            case "-":
+                res = a - b;
+                break;
+            case "*":
+                res = a * b;
+                break;
+            case "/":
+                if (b != 0) {
+                    res = a / b;
+                } else
+                    System.out.println("Ошибка!\nВы задали деление на 0");
+
+        }
+        System.out.println("Ответ -> " + (res) );
+    }
+
+}
