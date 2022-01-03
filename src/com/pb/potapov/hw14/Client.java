abf28ca5940ef41efe927d49ac11453b18dbb809
sergej-potapov-pb <sergej.potapov@privatbank.ua>
2022-01-04 00:55:07 +0200

    Multi-чат, все клиенты получают сообщения от друг друга

--- src/com/pb/potapov/hw14/Client.java
+++ src/com/pb/potapov/hw14/Client.java
@@ -1,40 +1,72 @@
 package com.pb.potapov.hw14;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
-    public static void main(String[] args) throws Exception {
+
+    // прием сообщений от сервера
+    static class AcceptMessages implements Runnable {
+        // поток для чтения данных
+        BufferedReader inMsg = null;
+
+        public AcceptMessages(BufferedReader inMsg) throws IOException {
+            this.inMsg = inMsg;
+        }
+
+        @Override
+        public void run() {
+            while (!Thread.interrupted()) {
+                try {
+
+                    String dataFromServer = inMsg.readLine();
+                    System.out.println(dataFromServer);
+
+                } catch (IOException ex) {
+                    // ignore
+                    break;
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
         System.out.println("Клиент стартовал");
         String serverIp = "127.0.0.1";
         int serverPort = 9000;
-        System.out.println("Соединяемся с сервером " + serverIp + ":" + serverPort+"\n"+
-                           "Для завершения работы наберите - exit");
+        System.out.println("Соединяемся с сервером " + serverIp + ":" + serverPort + "\n" +
+                "Для завершения работы наберите - exit");
 
+        try {
+            Socket server = new Socket(serverIp, serverPort);
 
-        Socket server = new Socket(serverIp, serverPort);
-        BufferedReader inServer = new BufferedReader(new InputStreamReader(server.getInputStream()));
-        PrintWriter outServer = new PrintWriter(server.getOutputStream(), true);
-        BufferedReader inConsole = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader inServer = new BufferedReader(new InputStreamReader(server.getInputStream()));
+            PrintWriter outServer = new PrintWriter(server.getOutputStream(), true);
+            BufferedReader inConsole = new BufferedReader(new InputStreamReader(System.in));
 
-        String dataFromUser, dataFromServer;
-        dataFromServer = inServer.readLine();
-        System.out.println(dataFromServer);
+            String dataFromUser;
 
-        // Основной цикл отправки сообщений серверу
-        while ((dataFromUser = inConsole.readLine()) != null) {
-            outServer.println(dataFromUser);
-            dataFromServer = inServer.readLine();
-            System.out.println(dataFromServer);
-            if ("exit".equalsIgnoreCase(dataFromUser)) {
-                break;
+            Thread AcceptMessagesThread = new Thread(new AcceptMessages(inServer));
+            AcceptMessagesThread.start();
+
+            // Основной цикл отправки сообщений серверу
+            while ((dataFromUser = inConsole.readLine()) != null) {
+                outServer.println(dataFromUser);
+                if ("exit".equalsIgnoreCase(dataFromUser)) {
+                    break;
+                }
             }
-        }
-        outServer.close();
-        inServer.close();
-        outServer.close();
-        server.close();
+            AcceptMessagesThread.interrupt();
+            outServer.close();
+            inServer.close();
+            server.close();
+        } catch (IOException ex) {
+            System.out.println("Сервер " + serverIp + ":" + serverPort + " не отвечает");
+            return;
+        };
     }
 }
--- src/com/pb/potapov/hw14/Server.java
+++ src/com/pb/potapov/hw14/Server.java
@@ -8,42 +8,144 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
+
+    static class RouteMessages implements Runnable {
+        private final Queue<String> msgQueue;
+        LinkedList<Handler> clients = new LinkedList<>();
+
+        public RouteMessages(Queue<String> msgQueue, LinkedList<Handler> clients) {
+            this.msgQueue = msgQueue;
+            this.clients = clients;
+        }
+
+
+        @Override
+        public void run() {
+            while (!Thread.interrupted()) {
+                try {
+
+                    sendAll();
+
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                    break;
+                }
+            }
+        }
+
+        private void sendAll() throws InterruptedException {
+            synchronized (msgQueue) {
+                if (msgQueue.isEmpty()) { // Если пуста, надо ждать
+
+                    msgQueue.wait();
+
+                }
+
+
+                if (!msgQueue.isEmpty()) {
+                    String tmpStr = msgQueue.poll();
+                    if (!tmpStr.isEmpty()) {
+                        System.out.println(tmpStr);
+                        // очистка списка от закрытых соединений
+                        for (int r = 0; r < clients.size(); r++) {
+                            Handler handler = (Handler) clients.get(r);
+                            if (handler.getClientSocket().isClosed()) {
+                                clients.remove(r);
+                            }
+
+                        }
+
+                        // отправляем сообщения всем подключенным клиентам
+                        for (int i = 0; i < clients.size(); i++) {
+                            Handler handler = (Handler) clients.get(i);
+                            handler.outMsg.println(tmpStr);
+                        }
+                        // для отладки - удалить
+                        // System.out.println("clients count = " + clients.size());
+                    }
+                }
+
+                msgQueue.notifyAll();
+
+            }
+        }
+    }
+
+
     static class Handler implements Runnable {
         private final Socket clientSocket;
+        private final Queue<String> msgQueue;
+        // Максимальный размер очереди
+        private final int maxSize;
+        private String clientName = null;
+        // поток для отправки данных
+        public PrintWriter outMsg = null;
 
-        public Handler(Socket socket) {
-            this.clientSocket = socket;
+        public String getClientName() {
+            return clientName;
         }
+
+        public Handler(Socket socket, Queue<String> msgQueue, int maxSize) throws IOException {
+            this.clientSocket = socket;
+            this.msgQueue = msgQueue;
+            this.maxSize = maxSize;
+            this.outMsg = new PrintWriter(socket.getOutputStream(), true);
+
+        }
+
+        public Socket getClientSocket() {
+            return clientSocket;
+        }
+
+        public void setClientName(String clientName) {
+            this.clientName = clientName;
+        }
+
+        public void msgToQueue(String message) throws InterruptedException {
+            synchronized (msgQueue) { // обязательно synchronized
+
+                if (msgQueue.size() >= maxSize) {
+                    // Если очередь полна, то ждём
+                    msgQueue.wait();
+                }
+
+                // Добавили элемент в очередь.
+                msgQueue.add(message);
+
+                // Уведомили другой поток на случай, если он ждет
+                msgQueue.notifyAll();
+            }
+        }
+
         @Override
         public void run() {
             try {
                 // поток для чтения данных
                 BufferedReader inMsg = null;
-                // поток для отправки данных
-                PrintWriter outMsg = null;
 
-                System.out.println("Подключился клиент - "+Thread.currentThread().getName());
-                // создаем потоки для связи с клиентом
+                System.out.println("Подключился клиент - " + Thread.currentThread().getName());
+
                 inMsg = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                outMsg = new PrintWriter(clientSocket.getOutputStream(), true);
+
                 String clientMessage;
-                outMsg.println("Ваше имя на сервере : "+Thread.currentThread().getName());
+                outMsg.println("Ваше имя на сервере : " + Thread.currentThread().getName());
+                setClientName(Thread.currentThread().getName());
 
                 // цикл ожидания сообщений от клиента
-                //System.out.println("Ожидаем сообщений");
                 while ((clientMessage = inMsg.readLine()) != null) {
                     if ("exit".equalsIgnoreCase(clientMessage)) {
-                        System.out.println("Отключился клиент - "+Thread.currentThread().getName());
+                        System.out.println("Отключился клиент - " + Thread.currentThread().getName());
                         break;
                     }
                     LocalDateTime today = LocalDateTime.now();
-                    String msgStr =today.format(DateTimeFormatter.ofPattern("dd.MM.yyyy hh:mm"))+" ( "+Thread.currentThread().getName()+") -> " + clientMessage;
-                    outMsg.println(msgStr);
-                    System.out.println(msgStr);
+                    String msgStr = today.format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")) + " ( " + getClientName() + ") -> " + clientMessage;
+                    msgToQueue(msgStr);
                 }
 
                 // Закрываем все соединения c текущим клиентом
@@ -61,12 +163,15 @@ public class Server {
                 }
             }
         }
+
     }
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Сервер запущен");
-        int serverPort = 9000;
+        LinkedList<Handler> clients = new LinkedList<>();
+        LinkedList<String> msgQueue = new LinkedList<>();
+        int MaxSize = 20; // размер очереди сообщений
 
+        int serverPort = 9000;
 
         // серверный сокет
         ServerSocket serverSocket = null;
@@ -75,16 +180,21 @@ public class Server {
         try {
             serverSocket = new ServerSocket(serverPort);
         } catch (IOException e) {
-            System.out.println("Ошибка связывания с портом "+ serverPort);
+            System.out.println("Ошибка работы с портом " + serverPort);
             System.exit(-1);
         }
 
+        System.out.println("Сервер запущен");
+
         ExecutorService threadPool = Executors.newFixedThreadPool(5);
+        Thread RouteMessagesThread = new Thread(new RouteMessages(msgQueue, clients));
+        RouteMessagesThread.start();
 
         // В цикле ждем запроса клиента
         while (true) {
             Socket clientSocketWait = serverSocket.accept();
-            threadPool.submit(new Handler(clientSocketWait));
+            clients.add(new Handler(clientSocketWait, msgQueue, MaxSize));
+            threadPool.submit(clients.getLast());
         }
     }
 }
